ab614c667be8ea39671b78b8909f365ffafe1abb
chittaranjan19 <vrchittaranjan@gmail.com>
2016-12-28 21:44:04 +0530

    Update index.js
    implement jsPlumb connectors, without self-loops

--- index.js
+++ index.js
@@ -1,123 +1,62 @@
-var context;
-function drawCanvas()
-{
-	var canvas = document.createElement("CANVAS");
-	canvas.id = "canvas";
-	canvas.style.width = "100%";
-	canvas.style.height = "80%";
-	canvas.addEventListener('click', function(evt)
-	{
-		var mousePos = getMousePos(canvas, evt);
-		connectTwo(mousePos.x,mousePos.y);
-	
-	}, false);
 
-	document.body.appendChild(canvas);
+function makeDiv()
+{
+	var theDiv = document.createElement("DIV");
+	theDiv.id = "theDiv";
+	theDiv.style.width = "100%";		
+	theDiv.style.height = "100%";
+	
+	document.body.appendChild(theDiv);
 	addImages();
 }
 
+function connect(evt)
+{
+	imgObj = evt.target;
+	
+	activeSceneID = "img1";
+	connectionTo = imgObj.id;
+	if (activeSceneID != connectionTo)
+	{
+		jsPlumb.connect({
+		  source:activeSceneID, 
+		  target:connectionTo,
+		  anchors:[ "Right", "Left" ],
+		  paintStyle:{ stroke:"white", strokeWidth:1 },
+		  endpointStyle:{ fill: "black" }
+		});
+	}
+
+}
+
+
 function addImages()
 {
-  pics = ["5.jpg","6.jpg","4.jpg"];
-  posX = 75;
-  posY = 75;
+  div = document.getElementById("theDiv");
   
-  context = document.getElementById("canvas").getContext("2d");
-  for(i=0; i<pics.length; ++i)
-  {
-  	img = new Image();
-  	img.onclick = function(){window.alert(this.name)}
-  	img.src = pics[i];
-  	img.name = pics[i];
-  	img.onload = function(){
-  	context.drawImage(this,posX + 45*pics.indexOf(this.name),posY,25,25);
-  	};
-  }
+  img1 = new Image(); img1.src = "4.jpg";  img1.id = "img1";
+  img2 = new Image(); img2.src = "5.jpg";  img2.id = "img2";
+  img3 = new Image(); img3.src = "6.jpg";  img3.id = "img3";
+
+  img1.onload = function(){
+  	img1.style = "height:130px;width:130px;position:absolute;left:35%;top:20%;border-radius:15px;";
+  	div.appendChild(img1);
+  	
+  };
+
+  img2.onload = function(){
+        img2.style = "height:130px;width:130px;position:absolute;left:60%;top:30%;border-radius:15px;";
+  	div.appendChild(img2);
+  };
+  
+  img3.onload = function(){
+  	img3.style = "height:130px;width:130px;position:absolute;left:45%;top:50%;border-radius:15px;";
+  	div.appendChild(img3);
+  };
+
+  img1.onclick = connect;
+  img2.onclick = connect;
+  img3.onclick = connect;
 }
 
 
-
-function objectsInBetween(x1,y1,x2,y2)
-{
-	if(x2>x1)
-	{
-		var obj = Math.abs(parseInt((x2-x1)/45)- 1);
-	}
-	else
-	{
-		var obj = Math.abs(parseInt((x1-x2+25)/45) - 1);
-	}
-	return obj;
-}
-
-function connectTwo(x,y)
-{
-	var activex = 165; //Assuming the API gets the x and y of the active scene.
-	var activey = 75;
-
-	var currx = activex;
-	var curry = activey; //stands for current y, and not fish curry.
-	
-	var num_objs = objectsInBetween(currx,curry,x,y);
-	var context = document.getElementById("canvas").getContext("2d");
-	if(num_objs == 0)
-	{
-		if(x>currx)
-		{
-			drawLine(currx+25,curry+12.5,currx+45,curry+12.5,2,'white'); //hardcoded for now
-		}
-		else
-		{
-			drawLine(currx,curry+12.5,currx-45+25,curry+12.5,2,'white'); //hardcoded for now
-		}
-	}
-	
-	else
-	{
-		
-		var radius = (num_objs)*45;
-		var startAngle = Math.PI;
-		var endAngle = 2 * Math.PI;
-		var counterClockwise = false;
-
-		
-		if(x>currx)
-		{
-			context.beginPath();
-			context.arc(currx+45+12.5, curry, radius, startAngle, endAngle, counterClockwise); //hardcoded for now
-			context.lineWidth = 2;
-		}
-		else
-		{
-			context.beginPath();
-			context.arc(currx-45+12.5, curry, radius, startAngle, endAngle, counterClockwise); //hardcoded for now
-			context.lineWidth = 2;
-		}
-
-		// line color
-		context.strokeStyle = 'white';
-		context.stroke();
-	}	
-}
-
-
-function getMousePos(canvas, evt) 
-{
-	var rect = canvas.getBoundingClientRect();
-	var dict = {x: Math.round((evt.clientX-rect.left)/(rect.right-rect.left)*canvas.width),
-		y: Math.round((evt.clientY-rect.top)/(rect.bottom-rect.top)*canvas.height)};
-	return dict;
-}
-
-
-function drawLine( xstart, ystart, xend, yend, width, color)
-{
-  context.beginPath();
-  context.strokeStyle = color;
-  context.lineWidth = width;
-  context.moveTo(xstart, ystart);
-  context.lineTo(xend, yend);
-  context.stroke();
-  context.closePath();
-}
-       
